d6129b8057ef8b0266e60e3f226d042547df012f
Denis Kerec <denis.kerec@gmail.com>
2026-03-22 00:24:35 +0100

    new shit

--- configs/nvim/init.lua
+++ configs/nvim/init.lua
@@ -602,13 +602,10 @@ require('lazy').setup({
       local servers = {
         -- clangd = {},
         -- gopls = {},
-        -- pyright = {},
+        pyright = {},  -- Python
+        jdtls = {},    -- Java
+        sqlls = {},    -- SQL
         -- rust_analyzer = {},
-        --
-        -- Some languages (like typescript) have entire language plugins that can be useful:
-        --    https://github.com/pmizio/typescript-tools.nvim
-        --
-        -- But for many setups, the LSP (`ts_ls`) will work just fine
         -- ts_ls = {},
 
         stylua = {}, -- Used to format Lua code
